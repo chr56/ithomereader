49dd56e7dd40e3dc82bb22d34addd7352bf02eec
iKirby <ikirby5556660@gmail.com>
2019-03-14 21:41:29 +0800

    Prepare for Android Q

--- app/src/main/java/me/ikirby/ithomereader/ui/fragment/SettingsFragment.kt
+++ app/src/main/java/me/ikirby/ithomereader/ui/fragment/SettingsFragment.kt
@@ -8,7 +8,27 @@ import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreference
-import me.ikirby.ithomereader.*
+import me.ikirby.ithomereader.BaseApplication
+import me.ikirby.ithomereader.BuildConfig
+import me.ikirby.ithomereader.R
+import me.ikirby.ithomereader.SETTINGS_KEY_CHECK_UPDATE
+import me.ikirby.ithomereader.SETTINGS_KEY_FONT_SIZE
+import me.ikirby.ithomereader.SETTINGS_KEY_GO_FULL_SITE
+import me.ikirby.ithomereader.SETTINGS_KEY_LIB_ANDROIDX
+import me.ikirby.ithomereader.SETTINGS_KEY_LIB_BOTTOMSHEET
+import me.ikirby.ithomereader.SETTINGS_KEY_LIB_GLIDE
+import me.ikirby.ithomereader.SETTINGS_KEY_LIB_JSOUP
+import me.ikirby.ithomereader.SETTINGS_KEY_LIB_KOTLINCOROUTINES
+import me.ikirby.ithomereader.SETTINGS_KEY_LIB_MDC
+import me.ikirby.ithomereader.SETTINGS_KEY_LIB_PHOTOVIEW
+import me.ikirby.ithomereader.SETTINGS_KEY_LOAD_IMAGE_COND
+import me.ikirby.ithomereader.SETTINGS_KEY_LOGIN_ACCOUNT
+import me.ikirby.ithomereader.SETTINGS_KEY_SHOW_THUMB_COND
+import me.ikirby.ithomereader.SETTINGS_KEY_SWIPE_GESTURE
+import me.ikirby.ithomereader.SETTINGS_KEY_USERNAME
+import me.ikirby.ithomereader.SETTINGS_KEY_USER_HASH
+import me.ikirby.ithomereader.SETTINGS_KEY_USE_BOTTOM_NAV
+import me.ikirby.ithomereader.SETTINGS_KEY_WHITE_THEME
 import me.ikirby.ithomereader.task.UpdateCheckNotifyTask
 import me.ikirby.ithomereader.ui.dialog.LoginDialog
 import me.ikirby.ithomereader.ui.dialog.TimePreferenceDialog
@@ -26,26 +46,26 @@ class SettingsFragment : PreferenceFragmentCompat() {
             setPreferencesFromResource(R.xml.preferences, rootKey)
 
             val showThumbPreference = findPreference<ListPreference>(SETTINGS_KEY_SHOW_THUMB_COND)
-            showThumbPreference.summaryProvider = Preference.SummaryProvider<ListPreference> { preference ->
+            showThumbPreference?.summaryProvider = Preference.SummaryProvider<ListPreference> { preference ->
                 preference.entries[preference.findIndexOfValue(preference.value)]
             }
 
             val loadImagePreference = findPreference<ListPreference>(SETTINGS_KEY_LOAD_IMAGE_COND)
-            loadImagePreference.summaryProvider = Preference.SummaryProvider<ListPreference> { preference ->
+            loadImagePreference?.summaryProvider = Preference.SummaryProvider<ListPreference> { preference ->
                 preference.entries[preference.findIndexOfValue(preference.value)]
             }
 
             val fontSizePreference = findPreference<ListPreference>(SETTINGS_KEY_FONT_SIZE)
-            fontSizePreference.summaryProvider = Preference.SummaryProvider<ListPreference> { preference ->
+            fontSizePreference?.summaryProvider = Preference.SummaryProvider<ListPreference> { preference ->
                 preference.entries[preference.findIndexOfValue(preference.value)]
             }
 
 
             val loginAccount = findPreference<Preference>(SETTINGS_KEY_LOGIN_ACCOUNT)
             if (preferences.contains(SETTINGS_KEY_USERNAME)) {
-                loginAccount.title = preferences.getString(SETTINGS_KEY_USERNAME, getString(R.string.login_title))
+                loginAccount?.title = preferences.getString(SETTINGS_KEY_USERNAME, getString(R.string.login_title))
             }
-            loginAccount.onPreferenceClickListener = Preference.OnPreferenceClickListener { preference ->
+            loginAccount?.onPreferenceClickListener = Preference.OnPreferenceClickListener { preference ->
                 var loginInfo: String? = null
                 if (preferences.contains(SETTINGS_KEY_USER_HASH)) {
                     loginInfo = "y"
@@ -60,18 +80,18 @@ class SettingsFragment : PreferenceFragmentCompat() {
             val swipeBackSwitch = findPreference<SwitchPreference>(SETTINGS_KEY_SWIPE_GESTURE)
             val useBottomNavSwitch = findPreference<SwitchPreference>(SETTINGS_KEY_USE_BOTTOM_NAV)
 
-            oStyleLightSwitch.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, _ ->
-                activity!!.setResult(Activity.RESULT_OK)
+            oStyleLightSwitch?.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, _ ->
+                activity?.setResult(Activity.RESULT_OK)
                 true
             }
 
-            swipeBackSwitch.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, _ ->
-                activity!!.setResult(Activity.RESULT_OK)
+            swipeBackSwitch?.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, _ ->
+                activity?.setResult(Activity.RESULT_OK)
                 true
             }
 
-            useBottomNavSwitch.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, _ ->
-                activity!!.setResult(Activity.RESULT_OK)
+            useBottomNavSwitch?.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, _ ->
+                activity?.setResult(Activity.RESULT_OK)
                 true
             }
 
@@ -84,49 +104,49 @@ class SettingsFragment : PreferenceFragmentCompat() {
             val libBottomSheet = findPreference<Preference>(SETTINGS_KEY_LIB_BOTTOMSHEET)
             val libKotlinxCoroutines = findPreference<Preference>(SETTINGS_KEY_LIB_KOTLINCOROUTINES)
 
-            goFullSite.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            goFullSite?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
                 openInCustomTabs("https://www.ithome.com")
                 true
             }
 
-            libAndroidX.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            libAndroidX?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
                 openInCustomTabs("https://developer.android.com/topic/libraries/support-library/androidx-rn")
                 true
             }
 
-            libMdc.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            libMdc?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
                 openInCustomTabs("https://github.com/material-components/material-components-android")
                 true
             }
 
-            libJsoup.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            libJsoup?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
                 openInCustomTabs("https://jsoup.org")
                 true
             }
 
-            libPhotoView.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            libPhotoView?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
                 openInCustomTabs("https://github.com/chrisbanes/PhotoView")
                 true
             }
 
-            libGlide.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            libGlide?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
                 openInCustomTabs("https://github.com/bumptech/glide")
                 true
             }
 
-            libBottomSheet.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            libBottomSheet?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
                 openInCustomTabs("https://github.com/krossovochkin/BottomSheetMenu")
                 true
             }
 
-            libKotlinxCoroutines.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            libKotlinxCoroutines?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
                 openInCustomTabs("https://github.com/Kotlin/kotlinx.coroutines/tree/master/ui/kotlinx-coroutines-android")
                 true
             }
 
             val checkForUpdate = findPreference<Preference>(SETTINGS_KEY_CHECK_UPDATE)
-            checkForUpdate.summary = getString(R.string.current_ver, BuildConfig.VERSION_NAME)
-            checkForUpdate.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            checkForUpdate?.summary = getString(R.string.current_ver, BuildConfig.VERSION_NAME)
+            checkForUpdate?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
                 UpdateCheckNotifyTask(true).execute()
                 true
             }
