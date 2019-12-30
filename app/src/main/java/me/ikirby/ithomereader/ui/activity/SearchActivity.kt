12072ee307f729fc6300171d124242503cc11c95
iKirby <ikirby5556660@gmail.com>
2019-12-30 17:46:36 +0800

    Fix search loading indicator invisible

--- app/src/main/java/me/ikirby/ithomereader/ui/activity/SearchActivity.kt
+++ app/src/main/java/me/ikirby/ithomereader/ui/activity/SearchActivity.kt
@@ -5,8 +5,6 @@ import android.os.Build
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
-import androidx.core.view.ViewCompat
-import androidx.core.view.updatePadding
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.list_layout.*
 import kotlinx.coroutines.Dispatchers
@@ -26,7 +24,6 @@ import me.ikirby.ithomereader.ui.util.ToastUtil
 import me.ikirby.ithomereader.ui.util.UiUtil
 import me.ikirby.ithomereader.ui.widget.OnBottomReachedListener
 import java.util.ArrayList
-import kotlin.math.roundToInt
 
 class SearchActivity : BaseActivity() {
 
@@ -79,17 +76,11 @@ class SearchActivity : BaseActivity() {
     }
 
     override fun initView() {
-        setContentView(R.layout.list_layout)
+        setContentView(R.layout.activity_search)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             window.navigationBarColor = Color.TRANSPARENT
             swipe_refresh.systemUiVisibility =
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-            ViewCompat.setOnApplyWindowInsetsListener(swipe_refresh) { v, insets ->
-                v.updatePadding(top = insets.systemWindowInsets.top)
-                swipe_refresh.setProgressViewOffset(false, 0, (insets.systemWindowInsets.top * 1.3).roundToInt())
-                list_view.updatePadding(bottom = insets.systemWindowInsets.bottom)
-                insets
-            }
         }
     }
 
