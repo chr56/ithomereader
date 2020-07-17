508b6784bc4f48696771f7cee26b24ed7c95820f
iKirby <ikirby5556660@gmail.com>
2020-07-17 21:59:38 +0800

    Remove check for update button on UnknownExceptionHandlerActivity

--- app/src/main/java/me/ikirby/ithomereader/ui/activity/UnknownExceptionHandlerActivity.kt
+++ app/src/main/java/me/ikirby/ithomereader/ui/activity/UnknownExceptionHandlerActivity.kt
@@ -5,17 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.unknown_exception_message.*
 import me.ikirby.ithomereader.KEY_STACK_TRACE
 import me.ikirby.ithomereader.R
-import me.ikirby.ithomereader.task.UpdateCheckNotifyTask
 
 class UnknownExceptionHandlerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.unknown_exception_message)
-
-        btn_check_for_update.setOnClickListener {
-            UpdateCheckNotifyTask(true).execute()
-        }
 
         val stackTrace = intent.getStringExtra(KEY_STACK_TRACE)
         if (stackTrace != null) {
