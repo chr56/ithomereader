17a3c9eab2a4225a1b98a46d393ed20e7b7fcd33
chr56 <N/A>
2022-05-31 19:27:54 +0800

    use viewBinding for post in SlideBannerAdapter
    & remove imports of kotlin-android-extension

--- app/src/main/java/me/ikirby/ithomereader/ui/adapter/SlideBannerAdapter.kt
+++ app/src/main/java/me/ikirby/ithomereader/ui/adapter/SlideBannerAdapter.kt
@@ -4,14 +4,13 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.view.*
-import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import kotlinx.android.synthetic.main.slide_image_view.view.*
 import me.ikirby.ithomereader.CLIP_TAG_NEWS_LINK
 import me.ikirby.ithomereader.KEY_TITLE
 import me.ikirby.ithomereader.KEY_URL
 import me.ikirby.ithomereader.R
+import me.ikirby.ithomereader.databinding.SlideImageViewBinding
 import me.ikirby.ithomereader.entity.Article
 import me.ikirby.ithomereader.ui.activity.ArticleActivity
 import me.ikirby.ithomereader.ui.activity.ImageViewerActivity
@@ -24,9 +23,10 @@ class SlideBannerAdapter(private val list: List<Article>, private val context: C
     RecyclerView.Adapter<SlideBannerAdapter.BannerItemViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BannerItemViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.slide_image_view, parent, false)
-        val holder = BannerItemViewHolder(view)
-        view.setOnClickListener {
+        val viewBinding = SlideImageViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val holder = BannerItemViewHolder(viewBinding)
+        // todo: move click listeners to onBindViewHolder
+        viewBinding.root.setOnClickListener {
             val (title, _, url) = list[holder.bindingAdapterPosition]
             val intent = Intent(context, ArticleActivity::class.java).apply {
                 putExtra(KEY_URL, url)
@@ -34,7 +34,7 @@ class SlideBannerAdapter(private val list: List<Article>, private val context: C
             }
             context.startActivity(intent)
         }
-        view.setOnLongClickListener {
+        viewBinding.root.setOnLongClickListener {
             showPopupMenu(list[holder.bindingAdapterPosition])
             true
         }
@@ -44,55 +44,55 @@ class SlideBannerAdapter(private val list: List<Article>, private val context: C
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: BannerItemViewHolder, position: Int) {
         val (_, _, _, thumb) = list[position]
-        Glide.with(context).asBitmap().load(thumb).into(holder.imageView)
-//        holder.title.text = title
+        holder.bind(thumb)
     }
 
     override fun getItemCount(): Int {
         return list.size
     }
 
-    inner class BannerItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var imageView: ImageView = itemView.slide_image
-//        var title: TextView = itemView.slide_title
-//        var bg: View = itemView.slide_bg
-//        var gradient: View = itemView.slide_gradient
-
+    inner class BannerItemViewHolder(private val viewBinding: SlideImageViewBinding) : RecyclerView.ViewHolder(viewBinding.root) {
+        internal fun bind(thumb: String?) {
+            Glide.with(context).asBitmap().load(thumb).into(viewBinding.slideImage)
+        }
     }
 
     private fun showPopupMenu(post: Article) {
-        UiUtil.showBottomSheetMenu(context, object : BottomSheetMenu.BottomSheetMenuListener {
-            override fun onCreateBottomSheetMenu(inflater: MenuInflater, menu: Menu) {
-                inflater.inflate(R.menu.main_context, menu)
-                if (post.thumb == null) {
-                    menu.removeItem(R.id.view_thumb)
+        UiUtil.showBottomSheetMenu(
+            context,
+            object : BottomSheetMenu.BottomSheetMenuListener {
+                override fun onCreateBottomSheetMenu(inflater: MenuInflater, menu: Menu) {
+                    inflater.inflate(R.menu.main_context, menu)
+                    if (post.thumb == null) {
+                        menu.removeItem(R.id.view_thumb)
+                    }
                 }
-            }
 
-            override fun onBottomSheetMenuItemSelected(item: MenuItem) {
-                when (item.itemId) {
-                    R.id.share -> {
-                        val share = Intent(Intent.ACTION_SEND).apply {
-                            putExtra(Intent.EXTRA_TEXT, post.title + "\n" + post.url)
-                            type = "text/plain"
-                        }
-                        context.startActivity(
-                            Intent.createChooser(
-                                share,
-                                context.getString(R.string.share) + " " + post.title
+                override fun onBottomSheetMenuItemSelected(item: MenuItem) {
+                    when (item.itemId) {
+                        R.id.share -> {
+                            val share = Intent(Intent.ACTION_SEND).apply {
+                                putExtra(Intent.EXTRA_TEXT, post.title + "\n" + post.url)
+                                type = "text/plain"
+                            }
+                            context.startActivity(
+                                Intent.createChooser(
+                                    share,
+                                    context.getString(R.string.share) + " " + post.title
+                                )
                             )
-                        )
-                    }
-                    R.id.copy_link -> copyToClipboard(CLIP_TAG_NEWS_LINK, post.url)
-                    R.id.view_thumb -> {
-                        val intent = Intent(context, ImageViewerActivity::class.java).apply {
-                            putExtra(KEY_URL, post.thumb)
                         }
-                        context.startActivity(intent)
+                        R.id.copy_link -> copyToClipboard(CLIP_TAG_NEWS_LINK, post.url)
+                        R.id.view_thumb -> {
+                            val intent = Intent(context, ImageViewerActivity::class.java).apply {
+                                putExtra(KEY_URL, post.thumb)
+                            }
+                            context.startActivity(intent)
+                        }
+                        R.id.open_in_browser -> openLinkInBrowser(context, post.url)
                     }
-                    R.id.open_in_browser -> openLinkInBrowser(context, post.url)
                 }
             }
-        })
+        )
     }
 }
