21b5dd85550d971b0bb8719b833ab01140bb5efb
chr56 <N/A>
2022-05-31 19:23:31 +0800

    use viewBinding for post in LivePostListAdapter
    & remove imports of kotlin-android-extension

--- app/src/main/java/me/ikirby/ithomereader/ui/adapter/LivePostListAdapter.kt
+++ app/src/main/java/me/ikirby/ithomereader/ui/adapter/LivePostListAdapter.kt
@@ -4,18 +4,13 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import kotlinx.android.synthetic.main.live_msg_item.view.*
 import me.ikirby.ithomereader.KEY_URL
 import me.ikirby.ithomereader.LIVE_MSG_TYPE_IMAGE
-import me.ikirby.ithomereader.R
+import me.ikirby.ithomereader.databinding.LiveMsgItemBinding
 import me.ikirby.ithomereader.entity.LiveMsg
 import me.ikirby.ithomereader.ui.activity.ImageViewerActivity
-import java.util.*
-
 
 class LivePostListAdapter(
     private val list: ArrayList<LiveMsg>,
@@ -23,48 +18,46 @@ class LivePostListAdapter(
 ) : RecyclerView.Adapter<LivePostListAdapter.LiveMsgViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LiveMsgViewHolder {
-        val view = inflater.inflate(R.layout.live_msg_item, parent, false)
-        return LiveMsgViewHolder(view)
+        return LiveMsgViewHolder(LiveMsgItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
     override fun onBindViewHolder(holder: LiveMsgViewHolder, position: Int) {
         val (postTime, content, type) = list[position]
+        holder.bind(
+            LiveMsgModel(postTime, content, type)
+        )
+    }
 
-        if (type == LIVE_MSG_TYPE_IMAGE) {
-            holder.timelineDot.visibility = View.GONE
-            holder.timeText.visibility = View.GONE
-            holder.text.visibility = View.GONE
-            holder.imageContainer.visibility = View.VISIBLE
-            Glide.with(inflater.context).load(content).into(holder.imageContainer)
-            holder.imageContainer.setOnClickListener {
-                val intent = Intent(inflater.context, ImageViewerActivity::class.java).apply {
-                    putExtra(KEY_URL, content)
+    override fun getItemCount(): Int = list.size
+
+    inner class LiveMsgViewHolder(private val viewBinding: LiveMsgItemBinding) : RecyclerView.ViewHolder(viewBinding.root) {
+
+        internal fun bind(data: LiveMsgModel) {
+            if (data.type == LIVE_MSG_TYPE_IMAGE) {
+                viewBinding.timelineDot.visibility = View.GONE
+                viewBinding.postTime.visibility = View.GONE
+                viewBinding.postTxt.visibility = View.GONE
+                viewBinding.postImageContainer.visibility = View.VISIBLE
+                Glide.with(inflater.context).load(data.content).into(viewBinding.postImageContainer)
+                viewBinding.postImageContainer.setOnClickListener {
+                    val intent = Intent(inflater.context, ImageViewerActivity::class.java).apply {
+                        putExtra(KEY_URL, data.content)
+                    }
+                    inflater.context.startActivity(intent)
                 }
-                inflater.context.startActivity(intent)
-            }
-        } else {
-            holder.timelineDot.visibility = View.VISIBLE
-            holder.timeText.visibility = View.VISIBLE
-            holder.imageContainer.visibility = View.GONE
-            if (content == "") {
-                holder.text.visibility = View.GONE
             } else {
-                holder.text.visibility = View.VISIBLE
-                holder.text.text = content
+                viewBinding.timelineDot.visibility = View.VISIBLE
+                viewBinding.postTime.visibility = View.VISIBLE
+                viewBinding.postImageContainer.visibility = View.GONE
+                if (data.content == "") {
+                    viewBinding.postTxt.visibility = View.GONE
+                } else {
+                    viewBinding.postTxt.visibility = View.VISIBLE
+                    viewBinding.postTxt.text = data.content
+                }
+                viewBinding.postTime.text = data.postTime
             }
-            holder.timeText.text = postTime
         }
     }
-
-    override fun getItemCount(): Int {
-        return list.size
-    }
-
-    inner class LiveMsgViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var timelineDot: ImageView = itemView.timeline_dot
-        var timeText: TextView = itemView.post_time
-        var text: TextView = itemView.post_txt
-        var imageContainer: ImageView = itemView.post_image_container
-
-    }
+    internal data class LiveMsgModel(val postTime: String?, val content: String, val type: Int)
 }
