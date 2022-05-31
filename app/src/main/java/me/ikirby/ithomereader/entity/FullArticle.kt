5e20562026812561eded4fe9b1cbc309109856e6
chr56 <N/A>
2022-05-31 20:21:22 +0800

    use correct new package in 'kotlin-parcelize'
    kotlinx.android.parcel.Parcelize -> kotlinx.parcelize.Parcelize

--- app/src/main/java/me/ikirby/ithomereader/entity/FullArticle.kt
+++ app/src/main/java/me/ikirby/ithomereader/entity/FullArticle.kt
@@ -1,7 +1,7 @@
 package me.ikirby.ithomereader.entity
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class FullArticle(
