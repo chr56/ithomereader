c9ba47e1077044b81f6431ea6c3bde4abca09d6f
chr56 <->
2025-06-12 15:53:31 +0800

    [release] v5.3.0-dev2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
         targetSdk = 34
 
         versionCode = 194
-        versionName = "5.3.0-dev1"
+        versionName = "5.3.0-dev2"
 
         resourceConfigurations += setOf("zh-rCN")
         versionNameSuffix = "-" + getGitHash(true)
